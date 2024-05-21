2c68e00b22bacfea227ed48a6553a93f810fbee8
tharsikan1999 <tharsikanking@gmail.com>
2024-05-21 12:36:05 +0530

    add loading effect

---
+++ src/Components/Loader.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect } from "react";
+import JImage from "../assets/Img/Loading Img/J.png";
+import XImage from "../assets/Img/Loading Img/X.png";
+import GImage from "../assets/Img/Loading Img/G.png";
+
+function Loader() {
+  useEffect(() => {
+    const resetAnimation = () => {
+      const chars = document.querySelectorAll(
+        ".char"
+      ) as NodeListOf<HTMLElement>;
+      chars.forEach((char, index) => {
+        char.style.opacity = "0"; // Reset opacity
+        char.style.animation = "none"; // Reset animation
+        setTimeout(() => {
+          char.style.opacity = "1"; // Set opacity to trigger animation
+          char.style.animation = "slideIn 0.4s ease forwards"; // Apply animation
+        }, index * 500); // Add delay based on index
+      });
+    };
+
+    // Call the resetAnimation function initially
+    resetAnimation();
+
+    // Reset the animation continuously
+    const interval = setInterval(resetAnimation, 1600); // Adjust the delay as needed
+
+    // Clean up function
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <style>
+        {`
+          #loader-wrapper {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(255, 255, 255, 0.7); /* semi-transparent white overlay */
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            z-index: 9999;
+          }
+
+          #loader {
+            font-size: 40px;
+          }
+
+          .char {
+            display: inline-block;
+            opacity: 0; /* Initially hide each character */
+            animation: slideIn 0.1s ease forwards; /* Apply animation */
+          }
+
+          @keyframes slideIn {
+            0% {
+              transform: translateY(-100%);
+              opacity: 0;
+            }
+            100% {
+              transform: translateY(0);
+              opacity: 1;
+            }
+          }
+        `}
+      </style>
+      <div id="loader-wrapper">
+        <div id="loader">
+          <img className="char" src={JImage} alt="JImage" />
+          <img className="char" src={XImage} alt="XImage" />
+          <img className="char" src={GImage} alt="GImage" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Loader;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,25 +1,47 @@
 import { useNavigate } from "react-router-dom";
 import Logo from "../assets/Img/JXG_logo.png";
+import Loader from "../Components/Loader";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Simulating a delay to demonstrate loading
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Adjust the delay as needed
+
+    // Clean up function
+    return () => {
+      // Additional cleanup if needed
+    };
+  }, []);
   const navigate = useNavigate();
   return (
-    <div className="bg-background-img min-h-screen w-full bg-center bg-no-repeat bg-cover">
-      <div className="h-20 flex justify-between items-center">
-        <img
-          src={Logo}
-          alt="JXG Logo"
-          className="m-3 h-10 cursor-pointer  ease-in-out transform hover:scale-110 transition-all duration-700  "
-        />
-        <button
-          onClick={() => navigate("/candidates")}
-          type="button"
-          className="text-white bg-yellow-500 bg-gradient-to-br first-line: from-CYellow to-orange-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-lg text-xl mr-5 font-semibold px-8 h-12 text-center transition-transform duration-300 ease-in-out transform hover:scale-110"
-        >
-          Login
-        </button>
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="bg-background-img min-h-screen w-full bg-center bg-no-repeat bg-cover">
+            <div className="h-20 flex justify-between items-center">
+              <img
+                src={Logo}
+                alt="JXG Logo"
+                className="m-3 h-10 cursor-pointer  ease-in-out transform hover:scale-110 transition-all duration-700  "
+              />
+              <button
+                onClick={() => navigate("/candidates")}
+                type="button"
+                className="text-white bg-yellow-500 bg-gradient-to-br first-line: from-CustomYellow to-orange-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-lg text-xl mr-5 font-semibold px-8 h-12 text-center transition-transform duration-300 ease-in-out transform hover:scale-110"
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
